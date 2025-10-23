866aead30bce999cdea0aa867147686937951125
Kamilla170 <kamilaimanalieva7@gmail.com>
2025-10-23 22:58:03 +0300

    Create user_states.py

---
+++ states/user_states.py
@@ -0,0 +1,19 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class PlantStates(StatesGroup):
+    """Состояния для работы с растениями"""
+    waiting_question = State()
+    editing_plant_name = State()
+    choosing_plant_to_grow = State()
+    planting_setup = State()
+    waiting_growing_photo = State()
+    adding_diary_entry = State()
+    onboarding_welcome = State()
+    onboarding_demo = State()
+    onboarding_quick_start = State()
+    waiting_state_update_photo = State()
+
+class FeedbackStates(StatesGroup):
+    """Состояния для обратной связи"""
+    choosing_type = State()
+    writing_message = State()
