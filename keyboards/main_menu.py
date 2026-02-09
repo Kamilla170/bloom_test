168ad90e7a1b7f5b39a3950b0e51001322ef4339
Kamilla170 <kamilaimanalieva7@gmail.com>
2026-02-09 18:12:53 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -16,7 +16,7 @@ def main_menu():
             InlineKeyboardButton(text="📊 Статистика", callback_data="stats")
         ],
         [
-            InlineKeyboardButton(text="⭐ PRO подписка", callback_data="show_subscription"),
+            InlineKeyboardButton(text="⭐ Подписка", callback_data="show_subscription"),
             InlineKeyboardButton(text="📝 Обратная связь", callback_data="feedback")
         ],
         [
